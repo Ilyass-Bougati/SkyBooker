ac1eb368dc62e1c728f1dd092c7f9de43d1aeb5d
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-05-02 14:38:45 +0100

    Implemented JWT
    following this article : https://www.danvega.dev/blog/spring-security-jwt

--- server/src/main/java/skybooker/server/ServerApplication.java
+++ server/src/main/java/skybooker/server/ServerApplication.java
@@ -2,7 +2,10 @@ package skybooker.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import skybooker.server.config.RsaKeyProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class ServerApplication {
 
---
+++ server/src/main/java/skybooker/server/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package skybooker.server.config;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final RsaKeyProperties rsaKeys;
+
+    public SecurityConfig(RsaKeyProperties rsaKeys) {
+        this.rsaKeys = rsaKeys;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Note that this is not safe, CSRF shouldn't be disabled but should be configured
+        // But it's fine since we're disabling session management
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(request -> {request
+                    .requestMatchers("/api/v1/auth/**").permitAll()
+                    .anyRequest().authenticated();
+        });
+
+        // Configuring the resource server
+        http.oauth2ResourceServer(oauth2 -> {
+            oauth2.jwt(Customizer.withDefaults());
+        });
+
+        // creating a stateless session, JWT is stateless
+        http.sessionManagement(session -> {
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        });
+        http.httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+
+    // TODO : change this, it think it's obvious why. And add a password encoder
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("ilyass")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build()
+        );
+    }
+
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+}
