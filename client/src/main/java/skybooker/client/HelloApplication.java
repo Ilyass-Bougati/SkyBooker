81c3520f87bf7c30dd1d77595bc6ecf08509ae68
AmineSidki <amineyoutubw@gmail.com>
2025-05-03 17:27:39 +0100

    made the auth frontend

--- client/src/main/java/skybooker/client/HelloApplication.java
+++ client/src/main/java/skybooker/client/HelloApplication.java
@@ -3,6 +3,7 @@ package skybooker.client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1171, 708 , false , SceneAntialiasing.BALANCED);
+        stage.setTitle("Authentication for SkyBooker");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
