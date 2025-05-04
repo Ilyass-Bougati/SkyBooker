35e038abc650dcb9b7993bbc4b215f56b064105c
AmineSidki <amineyoutubw@gmail.com>
2025-05-04 21:40:03 +0100

    The fonts are really infuriating

--- client/src/main/java/skybooker/client/HelloApplication.java
+++ client/src/main/java/skybooker/client/HelloApplication.java
@@ -4,18 +4,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1171, 708 , false , SceneAntialiasing.BALANCED);
+
         stage.setTitle("Authentication for SkyBooker");
         stage.setScene(scene);
-        stage.setResizable(false);
+        stage.setResizable(true);
+
+        stage.setX(0);
+        stage.setY(0);
+
+        stage.setMinHeight(619);
+        stage.setMinWidth(834);
+
+        stage.setMaxHeight(708);
+        stage.setMaxWidth(1171);
+
+
         stage.show();
     }
 
--- client/src/main/java/skybooker/client/HelloController.java
+++ client/src/main/java/skybooker/client/HelloController.java
@@ -1,14 +1,12 @@
 package skybooker.client;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
