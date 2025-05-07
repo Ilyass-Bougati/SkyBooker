010061cfdbfb553a50042d89fb2fe2fc9fc843d0
abdellahmp <abdellah.darni.2018@gmail.com>
2025-05-07 13:01:33 +0100

    add the CategorieController

--- server/src/main/java/skybooker/server/repository/CategorieRepository.java
+++ server/src/main/java/skybooker/server/repository/CategorieRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import skybooker.server.entity.Categorie;
 
+import java.util.Optional;
+
 @Repository
 public interface CategorieRepository extends JpaRepository<Categorie, Long> {
+    Optional<Categorie> findByNom(String name);
 }
