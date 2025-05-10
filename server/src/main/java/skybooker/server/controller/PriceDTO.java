b228a8f6119a03ad580a052ed3cd9205bf7eec27
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-05-10 13:39:06 +0100

    Added a Vol price route

---
+++ server/src/main/java/skybooker/server/controller/PriceDTO.java
@@ -0,0 +1,12 @@
+package skybooker.server.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PriceDTO {
+    private float price;
+}
