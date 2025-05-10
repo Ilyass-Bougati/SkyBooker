4e2d6396976e1fd782c637b4b157d6070d8a5188
Ilyass-Bougati <ilyass.bougati4ys@gmail.com>
2025-05-10 12:52:42 +0100

    Tests are back and fixed compiling issues

---
+++ server/src/test/java/skybooker/server/ServerApplicationTests.java
@@ -0,0 +1,14 @@
+package skybooker.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello World");
+    }
+
+}
