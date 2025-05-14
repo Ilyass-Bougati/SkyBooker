3772148a32b952115ef71d8c5e53bb02cc5e6cd6
AmineSidki <amineyoutubw@gmail.com>
2025-05-14 17:07:32 +0100

    Client : I forgot to uncomment some lines

--- client/src/main/java/skybooker/client/HelloController.java
+++ client/src/main/java/skybooker/client/HelloController.java
@@ -44,14 +44,14 @@ public class HelloController {
     @FXML
     protected void onLogInButton() throws IOException
     {
-//        if (email.getText().isEmpty() || password.getText().isEmpty()) {return;}
-//
-//        try {
-//            Client.login(email.getText(), password.getText());
-//        } catch (IOException ex) {
-//           Logger.getLogger(HelloController.class.getName()).log(Level.SEVERE, null, ex);
-//            return;
-//        }
+        if (email.getText().isEmpty() || password.getText().isEmpty()) {return;}
+
+        try {
+            Client.login(email.getText(), password.getText());
+        } catch (IOException ex) {
+           Logger.getLogger(HelloController.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
 
         GeneralUtils.loadView("Landingpage-view.fxml");
     }
