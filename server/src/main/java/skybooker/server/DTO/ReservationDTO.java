217549ed6d94640da622e73cce3eefba1090f118
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-05-18 16:51:45 +0100

    Server : Made making a reservation creates billets and proper sieges

--- server/src/main/java/skybooker/server/DTO/ReservationDTO.java
+++ server/src/main/java/skybooker/server/DTO/ReservationDTO.java
@@ -3,10 +3,14 @@ package skybooker.server.DTO;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import skybooker.server.entity.Billet;
+import skybooker.server.entity.Passager;
 import skybooker.server.entity.Reservation;
 import skybooker.server.enums.EtatReservation;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -27,6 +31,17 @@ public class ReservationDTO {
     @NotNull
     private Long volId;
 
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PassagerData {
+        Long passagerId;
+        Long classId;
+    }
+
+    private Set<PassagerData> passagers = new HashSet<>();
+
     private LocalDateTime reservedAt;
 
     public ReservationDTO(Reservation reservation) {
@@ -36,5 +51,13 @@ public class ReservationDTO {
         setReservedAt(reservation.getReservedAt());
         setClientId(reservation.getClient().getId());
         setVolId(reservation.getVol().getId());
+
+        setPassagers(new HashSet<>());
+        for (Billet billet : reservation.getBillets()) {
+            getPassagers().add(new PassagerData(
+                    billet.getPassager().getId(),
+                    billet.getClasse().getId()
+            ));
+        }
     }
 }
--- server/src/main/java/skybooker/server/repository/BilletRepository.java
+++ server/src/main/java/skybooker/server/repository/BilletRepository.java
@@ -1,9 +1,18 @@
 package skybooker.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import skybooker.server.entity.Billet;
 
 @Repository
 public interface BilletRepository extends JpaRepository<Billet, Long> {
+
+    @Query("SELECT COUNT(b) = 0 FROM Billet b JOIN b.siege WHERE b.id = :siege AND b.reservation.vol.id = :volId")
+    Boolean siegeLibre(Long volId, int siege);
+
+    @Query("SELECT MAX(b.siege) FROM Billet b WHERE " +
+            "b.classe.id = :classeId AND b.reservation.vol.id = :volId")
+
+    Integer getMaxSiege(Long classeId, Long volId);
 }
