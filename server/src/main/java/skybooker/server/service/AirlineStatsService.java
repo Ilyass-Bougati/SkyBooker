304b421c438d809e93b49ba63d4a3f721ab8793d
abdellahmp <abdellah.darni.2018@gmail.com>
2025-05-21 20:44:06 +0100

    stats by time

--- server/src/main/java/skybooker/server/service/AirlineStatsService.java
+++ server/src/main/java/skybooker/server/service/AirlineStatsService.java
@@ -1,9 +1,12 @@
 package skybooker.server.service;
 
-
 import skybooker.server.DTO.stats.FlightStatsDTO;
 import skybooker.server.DTO.stats.RouteStatsDTO;
 import skybooker.server.DTO.stats.RouteRevenueStatsDTO;
+import skybooker.server.DTO.stats.PlaneOccupancyStatsDTO;
+import skybooker.server.DTO.stats.FlightThresholdStatsDTO;
+import skybooker.server.DTO.stats.ClientStatsDTO;
+
 
 import java.util.List;
 
@@ -11,4 +14,7 @@ public interface AirlineStatsService {
     List<FlightStatsDTO> getFlightOccupancyAndRevenueStats();
     List<RouteStatsDTO> getPassengerCountPerRoute();
     List<RouteRevenueStatsDTO> getTotalRevenuePerRoute();
+    PlaneOccupancyStatsDTO getPlaneOccupancyStats(Long avionId, Integer year, Integer quarter);
+    List<FlightThresholdStatsDTO> getFlightsBelowOccupancyThreshold(Integer year, Integer quarter, Double threshold);
+    List<ClientStatsDTO> getClientStatistics();
 }
