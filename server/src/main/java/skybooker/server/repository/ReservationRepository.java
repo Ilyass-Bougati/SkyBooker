a69807c3879a8720d03989fc03a6681a1243d3a5
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-05-29 21:40:44 +0100

    Server : added schedular purging of data

--- server/src/main/java/skybooker/server/repository/ReservationRepository.java
+++ server/src/main/java/skybooker/server/repository/ReservationRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import skybooker.server.entity.Billet;
 import skybooker.server.entity.Reservation;
 import skybooker.server.enums.EtatReservation;
@@ -13,7 +14,13 @@ import java.util.List;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    @Transactional
     @Modifying
     @Query("update Reservation r set r.etat=:etat where r.id=:reservationId")
     void modifyEtat(Long reservationId, EtatReservation etat);
+
+    @Transactional
+    @Modifying
+    @Query("delete Reservation r where r.etat=:etat")
+    void purgeEtat(EtatReservation etat);
 }
