5b121acad20f9a9640980f32cfeb714267b8c379
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-06-02 17:22:16 +0100

    Server : changed avion now

--- server/src/main/java/skybooker/server/controller/AvionController.java
+++ server/src/main/java/skybooker/server/controller/AvionController.java
@@ -3,17 +3,11 @@ package skybooker.server.controller;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import skybooker.server.DTO.AvionDTO;
-import skybooker.server.entity.Avion;
 import skybooker.server.service.AvionService;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/avion")
@@ -26,42 +20,21 @@ public class AvionController {
         this.avionService = avionService;
     }
 
-    @GetMapping("/")
-    public ResponseEntity<List<AvionDTO>> getAllAvion() {
-        List<Avion> avions = avionService.findAll();
-        List<AvionDTO> avionDTOs = avions.stream().map(AvionDTO::new).collect(Collectors.toList());
-        return ResponseEntity.ok(avionDTOs);
-    }
-
     @GetMapping("/{id}")
     public ResponseEntity<AvionDTO> getAvionById(@PathVariable Long id) {
-        Avion avion = avionService.findById(id);
-        if (avion == null) {
-            return ResponseEntity.notFound().build();
-        } else {
-            return ResponseEntity.ok(new AvionDTO(avion));
-        }
+        return ResponseEntity.ok(avionService.findById(id));
     }
 
     @PostMapping("/")
     @Secured("SCOPE_ROLE_ADMIN")
     public ResponseEntity<AvionDTO> createAvion(@RequestBody @Valid AvionDTO avionDTO) {
-        Avion savedAvion = avionService.createDTO(avionDTO);
-        if (savedAvion == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(new AvionDTO(savedAvion));
+        return ResponseEntity.ok(avionService.createDTO(avionDTO));
     }
 
     @PutMapping("/")
     @Secured("SCOPE_ROLE_ADMIN")
     public ResponseEntity<AvionDTO> updateAvion(@RequestBody @Valid AvionDTO avionDTO) {
-        Avion avion = avionService.updateDTO(avionDTO);
-        if (avion == null) {
-            return ResponseEntity.notFound().build();
-        } else {
-            return ResponseEntity.ok(new AvionDTO(avion));
-        }
+        return ResponseEntity.ok(avionService.updateDTO(avionDTO));
     }
 
     @DeleteMapping("/{id}")
