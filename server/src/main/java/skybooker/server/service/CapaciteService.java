c62d1c725b28f3073313aaf82cb079d6a328e334
abdellahmp <abdellah.darni.2018@gmail.com>
2025-06-03 16:18:59 +0100

    avion et capacite

--- server/src/main/java/skybooker/server/service/CapaciteService.java
+++ server/src/main/java/skybooker/server/service/CapaciteService.java
@@ -1,7 +1,12 @@
 package skybooker.server.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import skybooker.server.DTO.CapaciteDTO;
 import skybooker.server.entity.Capacite;
 
+import java.util.List;
+
 public interface CapaciteService extends CrudDTO<CapaciteDTO, Long>, CrudService<Capacite, Long> {
+    @Transactional(readOnly = true)
+    List<CapaciteDTO> findDTOsByAvionId(Long avionId);
 }
