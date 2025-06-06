82634e9e90a5545e3a5be3ee13e14b1e51df6b3f
AmineSidki <amineyoutubw@gmail.com>
2025-06-06 03:33:51 +0100

    Client : Preferences

--- client/src/main/java/skybooker/client/controller/PreferencesController.java
+++ client/src/main/java/skybooker/client/controller/PreferencesController.java
@@ -4,9 +4,19 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -23,6 +33,10 @@ public class PreferencesController {
 
     @FXML
     private Button nextButton ;
+
+    @FXML
+    private VBox scrollPaneBody ;
+
     public static boolean isComingFromBookPopup = false;
 
     @FXML
@@ -52,6 +66,18 @@ public class PreferencesController {
     @FXML
     protected void initialize()
     {
+        Platform.runLater(()->{
+            initializePassengers();
+            if(isComingFromBookPopup)
+            {
+                nextButton.setDisable(false);
+                nextButton.setOpacity(1);
+            }
+        });
+    }
+
+    private void initializePassengers()
+    {
         // fetching the passagers
         ObjectMapper mapper = new ObjectMapper();
         List<PassagerDTO> passagers;
@@ -61,19 +87,47 @@ public class PreferencesController {
             for (PassagerDTO passager : passagers) {
                 System.out.println(passager.getCIN());
             }
-            // TODO : Show the passagers
+            //Show the passagers  ;; Stop ordering me around >:(
+            for(PassagerDTO passager : passagers)
+            {
+                HBox container = new HBox();
+                container.setAlignment(Pos.CENTER);
+                container.setSpacing(40);
+
+                CheckBox checkBox = new CheckBox();
+                checkBox.setStyle("-fx-background-color: #EDEDED");
+
+                Text fName = new Text(passager.getPrenom());
+                fName.setFont(new Font("Roboto" , 20));
+
+                Text lName = new Text(passager.getNom());
+                lName.setFont(new Font("Roboto" , 20));
+
+                Text category = new Text("Category");
+                category.setFont(new Font("Roboto" , 20));
+
+                //TODO : figure out how to get the category of the passenger
+
+                StackPane stackPane = new StackPane();
+
+                ImageView icon = new ImageView(new Image(HelloApplication.class.getResource("/assets/icons/Edit.png").toExternalForm()));
+                Button button = new Button();
+                button.setMaxWidth(16);
+                button.setMinWidth(16);
+                button.setMaxHeight(16);
+                button.setMinHeight(16);
+                button.setOpacity(0);
+                button.setOnAction(_ -> loadPassengerEditor(PassengereditorController.Mode.EDIT));
+
+                stackPane.getChildren().addAll(icon , button);
+                container.getChildren().addAll(checkBox , fName , lName , category , stackPane);
+
+                scrollPaneBody.getChildren().addAll(container , new Separator());
+
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-        Platform.runLater(()->{
-            if(isComingFromBookPopup)
-            {
-                nextButton.setDisable(false);
-                nextButton.setOpacity(1);
-            }
-        });
     }
 
     private void loadPassengerEditor(PassengereditorController.Mode mode)
